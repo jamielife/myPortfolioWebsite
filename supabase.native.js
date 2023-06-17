91b13856f7980b39f3fefd2631f4683680ada668
Jamie Taylor <jvptaylor@hotmail.com>
2023-06-17 02:02:47 -0400

    added mobile specific supabase config file

--- supabase.native.js
+++ supabase.native.js
@@ -1,5 +1,6 @@
 import {createClient, SupabaseClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-native-url-polyfill/auto';
 
 export const API_URL = 'https://oqqkzhcqvfmgxrbgbauq.supabase.co';
 export const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xcWt6aGNxdmZtZ3hyYmdiYXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5MDU4MTgsImV4cCI6MjAwMjQ4MTgxOH0.c7aZAjU3Q8ST3y1kHYIgE8NlCbhkFSSSUHmtOoqaXp0';
