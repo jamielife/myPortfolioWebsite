aad216236407f7773b265580a3f07043cd0bc9bc
Jamie Taylor <jvptaylor@hotmail.com>
2023-07-08 14:14:59 -0400

    Tiles working properly

--- components/WorkTile.js
+++ components/WorkTile.js
@@ -1,40 +1,12 @@
-import {Text, AspectRatio, Stack, Pressable, Heading } from "native-base";
-import { Image } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState} from "react";
+import {Text, Image, AspectRatio, Stack, Pressable, Heading } from "native-base";
 
 function WorkTile({data}) {  
-    //console.warn(data)    
-
-    const setData = (id, value) => {
-        AsyncStorage.setItem(id, value, () => {
-            //console.warn(id + " + " + value + " set!")
-        })
-    }
-    
-    const getData = (id) => {
-        AsyncStorage.getItem(id).then(storage => {
-            //console.warn(storage)
-        }).catch(e => console.warn(e))
-    }
-    
-    const response = Image.prefetch(data.imageFull,()=>console.log('Image is being fetched')).then(   );
-
     return ( 
         <Pressable mt={5} shadow="2" rounded="lg" w={{ base: 96, md: 72, lg: 48 }} 
             _light={{ bg: "coolGray.50" }} _dark={{ bg: "gray.800" }} overflow={"hidden"}>
-            <AspectRatio 
-                ratio= {{ 
-                    base: 16/9,
-                    md: 16/9
-                }} height= {{
-                    base: 20,
-                    md: 20
-                }} >
-                <Image key={data.id} prefetch resizeMode="cover" source={{
+                <Image w={200} h={110} key={data.id} resizeMode="cover" source={{
                     uri: data.imageFull
                 }} alt={data.blurb} />
-            </AspectRatio>
             <Text bold position="absolute" color="coolGray.50" top="0" m={3} mt={2}>
                 {data.type.toUpperCase()}
             </Text>                       
